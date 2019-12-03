2a0b03d917303cf5a191958e0438e598d448a5f0
mariuszberner <mariuszberner@gmail.com>
2019-12-03 22:13:55 +0100

    Services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
-import {HEROES} from '../mock-heroes';
+// import {HEROES} from '../mock-heroes'; // poprzednia wersja bez uzycia serwisu
+import {HeroService} from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -12,15 +13,27 @@ export class HeroesComponent implements OnInit {
     id: 1,
     name: 'Windstorm'
  } */
-heroes = HEROES;
+//heroes = HEROES; // poprzednia wersja bez uzycia serwisu 
+heroes: Hero[];
+
 selectedHero:Hero;
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero):void {
     this.selectedHero = hero;
   }
+
+/*   getHeroes():void {
+    this.heroes = this.heroService.getHeroes(); 
+  } */
+getHeroes():void {
+  this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+}
+ 
 
 }
